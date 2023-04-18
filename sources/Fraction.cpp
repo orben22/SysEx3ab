0bf31a28df76b96a71cc4ce43f409929de413000
or ben ami <orbenami22@gmail.com>
2023-04-18 17:20:16 +0300

    empty methods

--- sources/Fraction.cpp
+++ sources/Fraction.cpp
@@ -1,5 +1,213 @@
-//
-// Created by or on 4/18/23.
-//
-
 #include "Fraction.hpp"
+
+namespace ariel {
+    //constructors
+    Fraction::Fraction(int numerator, int denominator) : numerator(numerator), denominator(denominator) {}
+
+    Fraction::Fraction(const Fraction &otherFraction) : numerator(otherFraction.numerator),
+                                                  denominator(otherFraction.denominator) {}
+
+    Fraction::Fraction() : numerator(0), denominator(1) {};
+
+    Fraction::~Fraction() = default;
+
+//plus methods
+    Fraction Fraction::operator+(const Fraction &other) const {
+        return {0, 0};
+    }
+
+    Fraction Fraction::operator+(float scalar) const {
+        return {0, 0};
+    }
+
+    Fraction operator+(float scalar, const Fraction &other) {
+        return {0, 0};
+    }
+
+//subtract methods
+    Fraction Fraction::operator-(const Fraction &other) const {
+        return {0, 0};
+    }
+
+    Fraction Fraction::operator-(float scalar) const {
+        return {0, 0};
+    }
+
+    Fraction operator-(float scalar, const Fraction &other) {
+        return {0, 0};
+    }
+
+    Fraction Fraction::operator-(const Fraction &other) {
+        return {0, 0};
+    }
+
+    Fraction Fraction::operator-() const {
+        return {0, 0};
+    }
+
+//multiply methods
+    Fraction Fraction::operator*(const Fraction &other) const {
+        return {0, 0};
+    }
+
+    Fraction Fraction::operator*(float scalar) const {
+        return {0, 0};
+    }
+
+    Fraction operator*(float scalar, const Fraction &other) {
+        return {0, 0};
+    }
+
+//divide methods
+    Fraction Fraction::operator/(const Fraction &other) const {
+        return {0, 0};
+    }
+
+    Fraction Fraction::operator/(float scalar) const {
+        return {0, 0};
+    }
+
+    Fraction operator/(float scalar, const Fraction &other) {
+        return {0, 0};
+    }
+
+    Fraction Fraction::operator/(const Fraction &other) {
+        return {0, 0};
+    }
+
+//comparison methods
+//greater than (>)
+    bool Fraction::operator>(const Fraction &other) const {
+        return true;
+    }
+
+    bool Fraction::operator>(float num) const {
+        return true;
+    }
+
+    bool operator>(float num, const Fraction &other) {
+        return true;
+    }
+
+//less than (<)
+    bool Fraction::operator<(const Fraction &other) const {
+        return true;
+    }
+
+    bool Fraction::operator<(float num) const {
+        return true;
+    }
+
+    bool operator<(float num, const Fraction &other) {
+        return true;
+    }
+
+//greater or equal (>=)
+    bool Fraction::operator>=(const Fraction &other) const {
+        return true;
+    }
+
+    bool Fraction::operator>=(float num) const {
+        return true;
+    }
+
+    bool operator>=(float num, const Fraction &other) {
+        return true;
+    }
+
+//less or equal (<=)
+    bool Fraction::operator<=(const Fraction &other) const {
+        return true;
+    }
+
+    bool Fraction::operator<=(float num) const {
+        return true;
+    }
+
+    bool operator<=(float num, const Fraction &other) {
+        return true;
+    }
+
+// equal (=)
+    Fraction &Fraction::operator=(const Fraction &other) {
+        return *this;
+    }
+
+    Fraction &Fraction::operator=(float number) {
+        return *this;
+    }
+
+    Fraction &Fraction::operator=(Fraction &&other) noexcept {
+        return *this;
+    }
+
+//is equal (==)
+    bool Fraction::operator==(const Fraction &other) const {
+        return true;
+    }
+
+    bool Fraction::operator==(float num) const {
+        return true;
+    }
+
+    bool operator==(float num, const Fraction &other) {
+        return true;
+    }
+
+//is not equal (!=)
+    bool Fraction::operator!=(const Fraction &other) const {
+        return true;
+    }
+
+    bool Fraction::operator!=(float num) const {
+        return true;
+    }
+
+    bool operator!=(float num, const Fraction &other) {
+        return true;
+    }
+//increment a fraction:
+
+//++this
+    Fraction &Fraction::operator++() {
+        return *this;
+    }
+
+//this++
+    const Fraction Fraction::operator++(int) {
+        return {0, 0};
+    }
+
+//--this
+    Fraction &Fraction::operator--() {
+        return *this;
+    }
+
+//this--
+    const Fraction Fraction::operator--(int) {
+        return {0, 0};
+    }
+
+//output method
+    std::ostream &operator<<(std::ostream &ostream, const Fraction &other) {
+        return ostream;
+    }
+
+//input method
+    std::istream &operator>>(std::istream &istream, Fraction &other) {
+        return istream;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- sources/Fraction.hpp
+++ sources/Fraction.hpp
@@ -1,14 +1,127 @@
-//
-// Created by or on 4/18/23.
-//
-
 #ifndef SYSEX3AB_FRACTION_H
 #define SYSEX3AB_FRACTION_H
 
+#include "ostream"
 
-class fraction {
+namespace ariel {
+    class Fraction {
+    private:// data members
+        int numerator;
+        int denominator;
+    public:
+        //constructors
+        Fraction(int numerator,int denominator);
 
-};
+        Fraction(const Fraction &otherFraction);
 
+        Fraction();
+        //destructor
+        ~Fraction();
+
+        //plus methods
+        Fraction operator+(const Fraction &other) const;
+
+        Fraction operator+(float num) const;
+
+        friend Fraction operator+(float num, const Fraction &other);
+
+        //subtract method
+        Fraction operator-(const Fraction &other) const;
+
+        Fraction operator-(float num) const;
+
+        friend Fraction operator-(float num, const Fraction &other);
+
+        Fraction operator-(const Fraction &other);
+
+        Fraction operator-() const;
+
+        //multiply methods
+        Fraction operator*(const Fraction &other) const;
+
+        Fraction operator*(float num) const;
+
+        friend Fraction operator*(float num, const Fraction &other);
+
+        //divide methods
+        Fraction operator/(const Fraction &other) const;
+
+        Fraction operator/(float num) const;
+
+        friend Fraction operator/(float num, const Fraction &other);
+
+        Fraction operator/(const Fraction &other);
+
+        //comparison methods:
+        //greater than (>)
+        bool operator>(const Fraction &other) const;
+
+        bool operator>(float num) const;
+
+        friend bool operator>(float num, const Fraction &other);
+
+        //less than (<)
+        bool operator<(const Fraction &other) const;
+
+        bool operator<(float num) const;
+
+        friend bool operator<(float num, const Fraction &other);
+
+        //greater or equal (>=)
+        bool operator>=(const Fraction &other) const;
+
+        bool operator>=(float num) const;
+
+        friend bool operator>=(float num, const Fraction &other);
+
+        //less or equal (<=)
+        bool operator<=(const Fraction &other) const;
+
+        bool operator<=(float num) const;
+
+        friend bool operator<=(float num, const Fraction &other);
+        //equal (=)
+        Fraction &operator=(const Fraction &other);
+
+        Fraction &operator=(float number);
+
+        Fraction &operator=(Fraction &&other) noexcept;
+        //is equal (==)
+        bool operator==(const Fraction &other) const;
+
+        bool operator==(float num) const;
+
+        friend bool operator==(float num, const Fraction &other);
+
+        //is not equal (!=)
+        bool operator!=(const Fraction &other) const;
+
+        bool operator!=(float num) const;
+
+        friend bool operator!=(float num, const Fraction &other);
+        //increment a fraction:
+
+        //++this
+        Fraction &operator++();
+
+        //this++
+        const Fraction operator++(int);
+
+        //--this
+        Fraction &operator--();
+
+        //this--
+        const Fraction operator--(int);
+
+
+        //output method
+        friend std::ostream &operator<<(std::ostream &ostream, const Fraction &other);
+
+        //input method
+        friend std::istream &operator>>(std::istream &istream, Fraction &other);
+
+
+    };
+}
 
 #endif //SYSEX3AB_FRACTION_H
